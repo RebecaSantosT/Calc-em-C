87a50d30099c193eab1fb37fa72ffa7b155e72fc
RebecaSantosT <57728637+RebecaSantosT@users.noreply.github.com>
2019-11-14 15:42:49 -0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main(int argc, char *argv[]) {
+
+int i,a,x;
+
+	system
+	
+	printf("-----------Vamos decobrir a sua idade----------------");
+	
+	printf("\n\n Insira o ano em que estamos \n\n");
+	scanf("%i",&i);
+	
+	printf("Insira o ano em que voce nasceu \n\n");
+	scanf("%i",&a);
+	
+	x= i - a;
+
+	printf("\n\n Sua idade e: %i",x);
+}
